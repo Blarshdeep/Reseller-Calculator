e5c2f7ce6297d2b1963d5e9670bd5af80759a28b
Blarshdeep <blarshgill@gmail.com>
2021-03-28 15:18:48 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Welcome! 
+print('Reseller Calculator v0.01 - Currently supporting'+"...")
+user1 = input('Are you selling on StockX or Goat? ')
+
+# StockX Seller Levels
+stockx_levels = [(1, 1.0925), (2, 1.09),(3, 1.085),(4, 1.08)]
+
+
+# Breaks into either StockX or Goat
+if (user1 == "StockX"):
+  item_val = input(str('What is the value of the item you are selling? ' ))
+  seller_lvl = input(str('What is your StockX Seller Level? (Only input the number, ex. 3) ' ))
+  
+
+
+
+
+
+
+stockx_levels = [(1, 1.0925), (2, 1.09),(3, 1.085),(4, 1.08)]
+
